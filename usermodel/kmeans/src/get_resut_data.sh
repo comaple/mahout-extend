3a213bf2f7ea7ac350fa40d2857d0334524c522e
zhangshengtao <zhangshengtao@kingsoft.com>
2014-03-24 14:20:53 +0800

    delete all the files

--- usermodel/kmeans/src/get_resut_data.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding : utf8
-# Inout : the result of the kmeans agrothm 
-# Output : the result of the result.txt
-
-import sys
-
-
--- usermodel/kmeans/src/runk-means.sh
+++
@@ -1,7 +0,0 @@
-
-dt=$(date +"%Y%m%d")
-#nohup mahout org.apache.mahout.clustering.syntheticcontrol.kmeans.Job -i /user/ksckd/usermodel/kmeans/input/* -o /user/ksckd/usermodel/kmeans/output/  -dm EuclideanDistanceSimilarity  -x 20 -ow true > kmeans_$dt.log &
-
-nohup mahout org.apache.mahout.clustering.syntheticcontrol.kmeans.Job --input /user/ksckd/usermodel/kmeans/input/* --output /user/ksckd/usermodel/kmeans/output/  --distanceMeasure org.apache.mahout.common.distance.SquaredEuclideanDistanceMeasure --maxIter 20 -t1 0.4 -t2 0.2 -ow -k 5 > kmeans_$dt.log&
-
-
