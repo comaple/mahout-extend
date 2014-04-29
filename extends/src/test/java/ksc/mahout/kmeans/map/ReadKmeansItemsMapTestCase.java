fa726f88df346856e8dfef6fd19c8e4724f5eea4
zhangshengtao <zhangshengtao@kingsoft.com>
2014-04-29 13:03:11 +0800

    modify the kmeans job

--- extends/src/test/java/ksc/mahout/kmeans/map/ReadKmeansItemsMapTestCase.java
+++ extends/src/test/java/ksc/mahout/kmeans/map/ReadKmeansItemsMapTestCase.java
@@ -2,9 +2,9 @@ package ksc.mahout.kmeans.map;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mrunit.MapDriver;
-import org.apache.hadoop.mrunit.MapReduceDriver;
-import org.apache.hadoop.mrunit.ReduceDriver;
+//import org.apache.hadoop.mrunit.MapDriver;
+//import org.apache.hadoop.mrunit.MapReduceDriver;
+//import org.apache.hadoop.mrunit.ReduceDriver;
 import org.apache.mahout.clustering.classify.WeightedVectorWritable;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,8 +14,8 @@ import org.junit.Test;
  * Created by ZhangShengtao on 14-3-20.
  */
 public class ReadKmeansItemsMapTestCase {
-    MapDriver<IntWritable, WeightedVectorWritable, Text, Text> mapDriver;
-    MapReduceDriver<IntWritable, WeightedVectorWritable, Text, Text, Text, Text> mapReduceDriver;
+//    MapDriver<IntWritable, WeightedVectorWritable, Text, Text> mapDriver;
+//    MapReduceDriver<IntWritable, WeightedVectorWritable, Text, Text, Text, Text> mapReduceDriver;
 
     @Before
     public void setUp() {
--- extends/src/test/java/ksc/mahout/kmeans/reduce/ReadKResultReduceTestCase.java
+++ extends/src/test/java/ksc/mahout/kmeans/reduce/ReadKResultReduceTestCase.java
@@ -1,11 +1,11 @@
 package ksc.mahout.kmeans.reduce;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mrunit.ReduceDriver;
+//import org.apache.hadoop.mrunit.ReduceDriver;
 
 /**
  * Created by ZhangShengtao on 14-3-20.
  */
 public class ReadKResultReduceTestCase {
-    ReduceDriver<Text, Text, Text, Text> reducedriver;
+//    ReduceDriver<Text, Text, Text, Text> reducedriver;
 }
